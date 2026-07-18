44737b262c1e3976731ecb11fcc7e5f24e351c45
Aman Raj <goluraj120.com@gmail.com>
2026-07-18 23:36:31 +0530

    add Quiz_Agent

--- app.py
+++ app.py
@@ -1,48 +1,10 @@
-# # import Teacher_UI 
-
-# import streamlit as st
-
-# # -----------------------------
-# # Page Configuration
-# # -----------------------------
-# st.set_page_config(
-#     page_title="AI Learning Assistant",
-#     page_icon="🤖",
-#     layout="wide"
-# )
-
-# # -----------------------------
-# # Sidebar
-# # -----------------------------
-# st.sidebar.title("🤖 AI Learning Assistant")
-
-# page = st.sidebar.selectbox(
-#     "Select Agent",
-#     [
-#         "Teacher Agent",
-#         "Quiz Agent",
-#         "Planner Agent"
-#     ]
-# )
-
-# # -----------------------------
-# # Load Selected Agent
-# # -----------------------------
-# if page == "Teacher Agent":
-#     import Teacher_UI
-
-# elif page == "Quiz Agent" :
-#     import Quiz_UI
-
-# elif page == "Planner Agent":
-#     import Planner_UI
 
 import streamlit as st
 from UI.Teacher_UI import teacher_page
 from UI.Quiz_UI import quiz_page
 
 st.set_page_config(
-        page_title="AI Placement Preparation Multi-Agent System",
+        page_title="🤖 AI Learning Assistant",
         page_icon="🧑‍💻",
         layout="wide"
     )
